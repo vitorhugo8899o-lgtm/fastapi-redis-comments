4ed641e7c80ce032580124f12aafc5fb261e6c56
Vítor <vitorhugo8899o@gmail.com>
2026-03-21 13:48:10 -0300

    fix: colocando features dentro de services para aproveitar a logica e dependencia

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,79 +1,23 @@
-from http import HTTPStatus
+from app.services.services_users import login_user, create_user
+
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from redis import asyncio as aioredis
-
 from app.redis_depends import get_redis
 from app.schemas.users import UserCreate, UserLogin
 
 router_users = APIRouter(prefix='/users', tags=['Users'])
 r = Annotated[aioredis.Redis, Depends(get_redis)]
-
+Login = Annotated[UserLogin, Depends(login_user)]
 
 @router_users.post('', status_code=201)
-async def create_user(user: UserCreate, r: r) -> dict:
-
-    email_key = f'user:email:{user.email}'
-
-    exist_email = await r.get(email_key)
-    if exist_email:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail='Este email já está cadastrado.',
-        )
-
-    id_user = await r.incr('users:count')
-
-    user_key = f'user:{id_user}'
-
-    user_dict = user.model_dump()
-    user_dict['id'] = id_user
-
-    async with r.pipeline(transaction=True) as pipe:
-        await pipe.hset(user_key, mapping=user_dict)
-
-        await pipe.set(email_key, id_user)
-
-        await pipe.sadd('users:all', id_user)
-
-        await pipe.execute()
-
-    return user_dict
-
+async def create(user: UserCreate, r: r) -> dict:
+    return create_user(user,r)
+    
 
 @router_users.post('/login', status_code=200)
-async def login_user(user: UserLogin, r: r) -> str:
-
-    exists = await r.get(f'user:email:{user.email}')
-
-    async with r.pipeline(transaction=True) as pipe:
-        #esperar ele executar para depois verificar os campos
-        
-        await pipe.hget(f'user:{exists}', 'email')
-
-        await pipe.hget(f'user:{exists}', 'password')
-
-        result = await pipe.execute()
-
-
-    if not exists:
-            raise HTTPException(
-                status_code=HTTPStatus.NOT_FOUND,
-                detail='Email não está cadastrado.'
-            )
-
-    if result[0] != user.email:
-            raise HTTPException(
-                status_code=HTTPStatus.FORBIDDEN,
-                detail='Email  incorreto'
-            )
-
-    elif result[1] != user.password:
-            raise HTTPException(
-                status_code=HTTPStatus.FORBIDDEN,
-                detail='senha incorreta'
-            )
-    return 'Usuario Logado!'
-
+async def login(user: UserLogin, r: r) -> str:
+    return login_user(r,user)
+    
 
